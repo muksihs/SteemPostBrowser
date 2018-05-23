817e167198b662b7366f8db2961a34de221c4c29
Leather Dog Muksihs <muksihs@gmail.com>
2018-05-22 20:23:47 -0400

    initial compilable/runnable app skeleton

---
+++ src/main/java/muksihs/steem/postbrowser/client/App.java
@@ -0,0 +1,12 @@
+package muksihs.steem.postbrowser.client;
+
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+
+public class App implements ScheduledCommand {
+
+	@Override
+	public void execute() {
+		
+	}
+
+}
